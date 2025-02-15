7c3af85a14d074d6329bdc4492d8ec57e58aa43b
devlongen <iago.mendonca_2005@hotmail.com>
2025-02-15 11:21:54 -0600

    learn: dependencies/config, input and output

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main(){
-    println!("Hello World!");
+use std::io;
+
+fn main() {
+    println!("Staring learn rust");
+
+    println!("Text:"); let mut text = String::new();
+    io::stdin()
+        .read_line(&mut text);
+    println!("You text:{}" ,text);
 }
